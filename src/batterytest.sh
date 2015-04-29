74aef734c5871eefbc44d0654da5cc4dee034a6e
Juuso Alasuutari <juuso.alasuutari@koneetkiertoon.fi>
2015-04-29 15:53:20 +0300

    Improve logging.

--- src/batterytest.sh
+++ src/batterytest.sh
@@ -13,18 +13,50 @@
 URL='https://www.youtube.com/'
 DIR="$HOME/batterytest"
 TMP="$DIR/tmp"
+RUN="$DIR/run"
 HTML="$TMP/page.html"
 
 #
-# Log timestamp, battery level, and
-# load average every five seconds.
+# Log timestamp and battery level every five seconds.
 #
-background_logger ()
+batt_logger ()
 {
   (
+    which ibam || exit 1
+    touch "$RUN/$BASHPID"
     while true; do
-      echo "$(date '+%s.%N') $(ibam -r --noprofile --percentbattery|head -1|cut -d' ' -f11) $(cat /proc/loadavg)" >> "$DIR/log"
-      sleep 6
+      echo "$(date '+%s.%N') $(ibam -r --noprofile --percentbattery|head -1|cut -d' ' -f11)" >> "$DIR/batt.log"
+      sleep 5
+    done
+  ) &>/dev/null &
+}
+
+#
+# Log timestamp and load average every five seconds.
+#
+load_logger ()
+{
+  (
+    [[ -f /proc/loadavg ]] || exit 1
+    touch "$RUN/$BASHPID"
+    while true; do
+      echo "$(date '+%s.%N') $(cat /proc/loadavg)" >> "$DIR/load.log"
+      sleep 5
+    done
+  ) &>/dev/null &
+}
+
+#
+# Log timestamp and available temperature readings every five seconds.
+#
+temp_logger ()
+{
+  (
+    ls /sys/class/thermal/thermal_zone*/temp || exit 1
+    touch "$RUN/$BASHPID"
+    while true; do
+      echo $(date '+%s.%N') $(cat /sys/class/thermal/thermal_zone*/temp) >> "$DIR/temp.log"
+      sleep 5
     done
   ) &>/dev/null &
 }
@@ -60,12 +92,15 @@ get_random_url_from_vids ()
 }
 
 mkdir -p "$TMP" || exit 1
+mkdir -p "$RUN" || exit 1
 
 if ! get_html_from_url; then
   exit 1
 fi
 
-background_logger
+batt_logger
+load_logger
+temp_logger
 
 while true; do
   get_vids_from_html
