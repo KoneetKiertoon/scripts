f9f3e7cf804dffca534a1d73b57a14f7ef29f8a7
Juuso Alasuutari <juuso.alasuutari@koneetkiertoon.fi>
2015-09-22 16:01:27 +0300

    basiccheck: Add ping test.

---
+++ src/basiccheck.d/30-network-ping.sh
@@ -0,0 +1,19 @@
+RUNNAME="ping test"
+
+runfile_exec()
+{
+  local ADDR MSG RET
+
+  if [[ $1 ]]
+  then
+    ADDR="$1"
+  else
+    ADDR='8.8.8.8'
+  fi
+
+  MSG="$(ping -c 1 "$ADDR" 2>&1)"
+  RET=$?
+
+  echo "$MSG"
+  return $RET
+}
