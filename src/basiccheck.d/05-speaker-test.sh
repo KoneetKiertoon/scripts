ce1521549f2706b25df4570d4cf7d9c45cae4141
Juuso Alasuutari <juuso.alasuutari@koneetkiertoon.fi>
2015-09-22 14:42:32 +0300

    Added basiccheck tests for speakers and video card.

---
+++ src/basiccheck.d/05-speaker-test.sh
@@ -0,0 +1,6 @@
+RUNNAME="speaker-test"
+
+runfile_exec()
+{
+  speaker-test -t wav -c2 -l1
+}
---
+++ src/basiccheck.d/06-video-card.sh
@@ -0,0 +1,22 @@
+RUNNAME="video-card"
+
+runfile_exec()
+{
+  local LSPCI="$(lspci)" MSG RET
+
+  if egrep 'VGA.*82845' <<< "$LSPCI"
+  then
+    MSG='Video card type "Intel 82845" is bad'
+    RET=1
+  elif egrep 'VGA.*SiS.*771/671' <<< "$LSPCI"
+  then
+    MSG='Video card type "SiS 771/671" is bad'
+    RET=1
+  else
+    MSG='Video card type seems to be OK.'
+    RET=0
+  fi
+
+  echo "$MSG"
+  return $RET
+}
