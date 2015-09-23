40bde36efa87162c9d8415a74929228ae1d9b2c8
Juuso Alasuutari <juuso.alasuutari@koneetkiertoon.fi>
2015-09-23 14:22:20 +0300

    basiccheck: Add CPU info test.

---
+++ src/basiccheck.d/40-cpu.sh
@@ -0,0 +1,13 @@
+RUNNAME="CPU info"
+
+runfile_exec()
+{
+  local MODEL CORES MAXMHZ
+
+  MODEL="$(grep '^model name' /proc/cpuinfo|sort -u|sed -r 's|^[^:]+:[[:blank:]]*||')"
+  CORES="$(grep '^cpu cores' /proc/cpuinfo|sort -u|sed -r 's|^[^:]+:[[:blank:]]*||')"
+  MAXMHZ="$(cpufreq-info -l|cut -d ' ' -f 2|sed -r 's/([0-9]{3})$/.\1/')"
+
+  echo "CPU model: $MODEL"$'\n'"CPU cores: $CORES"$'\n'"Max speed: $MAXMHZ MHz"
+  return 0
+}
